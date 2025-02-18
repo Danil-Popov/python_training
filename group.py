68a0f6c0dc0059b0b02672a16240727f51d470c5
Danil <frozone62@yandex.ru>
2025-02-18 20:06:19 +0300

    Использование вспомогательного класса в тесте

---
+++ group.py
@@ -0,0 +1,8 @@
+_author_ = 'Danil'
+
+class Group:
+
+    def __init__(self,name,header,footer):
+        self.name = name
+        self.header = header
+        self.footer = footer
--- test_add_group.py
+++ test_add_group.py
@@ -2,15 +2,18 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import NoSuchElementException
+from group import Group
 import unittest
+
+
+def open_home_page(wd):
+    wd.get("http://localhost/addressbook/")
+
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
+        self.wd.implicitly_wait(60)
 
     def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
@@ -24,20 +27,19 @@ class TestAddGroup(unittest.TestCase):
     def open_groups(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-
-    def create_group(self, wd, name, header, footer):
+    def create_group(self, wd, group):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
@@ -47,25 +49,23 @@ class TestAddGroup(unittest.TestCase):
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
+        open_home_page(wd)
         self.login(wd, username = "admin", password = "secret")
         self.open_groups(wd)
-        self.create_group(wd, name ="ghbdtn", header = "ghbdtn", footer ="ghbdtn")
+        self.create_group(wd, Group (name ="ghbdtn", header = "ghbdtn", footer ="ghbdtn"))
         self.return_to_groups(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
+        open_home_page(wd)
         self.login(wd, username = "admin", password = "secret")
         self.open_groups(wd)
-        self.create_group(wd, name = "", header = "", footer = "")
+        self.create_group(wd, Group ( name = "", header = "", footer = ""))
         self.return_to_groups(wd)
         self.logout(wd)
-
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
