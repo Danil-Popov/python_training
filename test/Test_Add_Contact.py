433bbb9c061733496d537eff5895c497348f66f9
Danil <frozone62@yandex.ru>
2025-03-01 11:26:19 +0300

    Выделен вспомогательный класс "SessionHelper"

--- test/Test_Add_Contact.py
+++ test/Test_Add_Contact.py
@@ -12,14 +12,14 @@ def app(request):
     return fixture
 
 def test_add_contact(app):
-    app.login(username = "admin", password = "secret")
+    app.session.login(username = "admin", password = "secret")
     app.create_contact(Contact(first_name = "danil",last_name = "popov", city= "ryazan city", number_phone = "89997778866", email = "frost@yandex.ru"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_contact(app):
-    app.login(username = "admin", password = "secret")
+    app.session.login(username = "admin", password = "secret")
     app.create_contact(Contact(first_name = "",last_name = "", city= "", number_phone = "", email = ""))
-    app.logout()
+    app.session.logout()
 
 
 
