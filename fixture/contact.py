d72f26f60e89fdbdf8356819893248cf056f9d83
Danil <frozone62@yandex.ru>
2025-03-01 11:46:53 +0300

    Выделен класс для работы с контактами

---
+++ fixture/contact.py
@@ -0,0 +1,34 @@
+_author_ = 'Danil'
+
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def create(self, contact):
+        # add new contact
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+        # fill contact group
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.city)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.number_phone)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        # creation contact
+        wd.find_element_by_xpath("//input[20]").click()
+        self.return_to_contacts()
+
+    def return_to_contacts(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
