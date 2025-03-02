6e5bcf39dc57967c62ace84c3aa0cb9a04a72b4c
Danil <frozone62@yandex.ru>
2025-03-02 15:02:06 +0300

    Валидация фикстуры перед каждым тестом

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,7 +1,4 @@
 _author_ = 'Danil'
 
 def test_delete_first_contact(app):
-
-    app.session.login(username = "admin", password = "secret")
-    app.contact.delete_first_contact()
-    app.session.logout()
+     app.contact.delete_first_contact()
