e41372ae57167a5f4804482e8a3c6b77189c7110
Danil <frozone62@yandex.ru>
2025-03-07 10:11:44 +0300

    Переименовал тест test_add_contact.py

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,7 +5,6 @@ from model.contact import Contact
 def test_add_contact(app):
     app.contact.create(Contact(first_name ="danil", last_name ="popov", city="ryazan city", number_phone ="89997778866", email ="frost@yandex.ru"))
 
-
 def test_add_empty_contact(app):
     app.contact.create(Contact(first_name ="", last_name ="", city="", number_phone ="", email =""))
 
