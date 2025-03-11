54746ec1030d6b1ec8dc13acc803a5dbc25c6dde
Danil <frozone62@yandex.ru>
2025-03-11 20:12:05 +0300

    Оптимизировал переходы между страницами для групп и контактов.

--- fixture/contact.py
+++ fixture/contact.py
@@ -5,9 +5,15 @@ class ContactHelper:
     def __init__(self, app):
         self.app = app
 
+    def open_contacts_page(self):
+        wd = self.app.wd
+        if not (wd.current_url.endswith("/addressbook") and len(wd.find_elements_by_name("add")) > 0):
+            wd.find_element_by_link_text("home").click()
+
     def create(self, contact):
         # add new contact
         wd = self.app.wd
+        self.open_contacts_page()
         wd.find_element_by_link_text("add new").click()
         self.fill_contact_group(contact)
         # creation contact
@@ -22,7 +28,6 @@ class ContactHelper:
             wd.find_element_by_name(field_name).send_keys(text)
 
     def fill_contact_group(self, contact):
-        wd = self.app.wd
         self.change_field_value("firstname",contact.first_name)
         self.change_field_value("lastname", contact.last_name)
         self.change_field_value("address", contact.city)
@@ -35,19 +40,21 @@ class ContactHelper:
 
     def delete_first_contact(self):
         wd = self.app.wd
+        self.open_contacts_page()
         self.select_first_contact()
         #submit deletion
         wd.find_element_by_xpath("//input[@value='Delete']").click()
 
     def edit_contact(self,contact):
         wd = self.app.wd
+        self.open_contacts_page()
         self.select_first_contact()
         #edit contact
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
         self.fill_contact_group(contact)
         #update contact
         wd.find_element_by_name("update").click()
-        wd.find_element_by_link_text("home page").click()
+        self.return_to_contacts()
 
     def return_to_contacts(self):
         wd = self.app.wd
